51ae5c6e07cdd84f349fd11154bd5d6e68e66067
TDSOJohn <giovanni.basso@icloud.com>
2021-02-19 18:14:35 +0100

    bugfixing

--- test/asl.c
+++ test/asl.c
@@ -3,7 +3,6 @@
 
 HIDDEN semd_t *semd_h, *semdFree_h = NULL;
 
-
 void insertSEMList(semd_t **semList, semd_t *sem_elem){
     if(*semList == NULL){
         /*Caso empty*/
@@ -17,103 +16,102 @@ void insertSEMList(semd_t **semList, semd_t *sem_elem){
         sem_elem->s_next = NULL;
     }
     else {
-        insertSEMList(&((*semList)->s_next), sem_elem);
+    	insertSEMList(&((*semList)->s_next), sem_elem);
     }
 }
 
-
 void initASL() {
-    static semd_t semdTable[MAXPROC];
-    int i;
-    for (i = 0; i < MAXPROC; i++) {
-        insertSEMList(&semdFree_h, &semdTable[i]);
-    }
+	static semd_t semdTable[MAXPROC];
+	int i;
+	for (i = 0; i < MAXPROC; i++) {
+		insertSEMList(&semdFree_h, &semdTable[i]);
+	}
     return;
 }
 
 int emptySEMList(semd_t **semlist_p){
-    if(*semlist_p == NULL)
-        return 1;
-    else
-        return 0;
+	if(*semlist_p == NULL)
+		return 1;
+	else
+		return 0;
 }
 
 semd_t* getSemd_rec(semd_t **semd_h, int* semAdd){
-    if(*semd_h == NULL){
-        return NULL; /*Il semaforo non esiste nella ASL*/
-    }
+	if(*semd_h == NULL){
+		return NULL; /*Il semaforo non esiste nella ASL*/
+	}
 
-    if((*semd_h)->s_semAdd == semAdd){
+	if((*semd_h)->s_semAdd == semAdd){
 
-        return *semd_h;
-    }
+		return *semd_h;
+	}
 
-    else
-        return getSemd_rec(&((*semd_h)->s_next), semAdd);
+	else
+		return getSemd_rec(&((*semd_h)->s_next), semAdd);
 }
 
 
 /*Cerca nella ASL il semaforo con chiave semAdd e restituisce un puntatore ad esso*/
 semd_t* getSemd(int *semAdd){
-    return getSemd_rec(&semd_h, semAdd);
+	return getSemd_rec(&semd_h, semAdd);
 }
 
 
 /*Estrae un SEM dalla lista dei semdFree e restituisce un puntatore a esso*/
 semd_t *allocSem(){
-    if(emptySEMList(&semdFree_h))
-        return NULL;
-    else{
-        semd_t *ptemp = semdFree_h;
-        semdFree_h = semdFree_h->s_next;
+	if(emptySEMList(&semdFree_h))
+		return NULL;
+	else{
+		semd_t *ptemp = semdFree_h;
+		semdFree_h = semdFree_h->s_next;
 
-        ptemp->s_next = NULL;
-        ptemp->s_semAdd = NULL;
-        ptemp->s_procQ = NULL;
-        return ptemp;
-    }
+		ptemp->s_next = NULL;
+		ptemp->s_semAdd = NULL;
+		ptemp->s_procQ = NULL;
+		return ptemp;
+	}
 }
 /*Viene inserito il PCB puntato da p nella coda dei processi bloccati associata al SEMD con chiave semAdd.
 Se il semaforo corrispondente non è presente nella ASL, alloca un nuovo SEMD dalla lista di quelli liberi (semdFree) e lo inserisce nella ASL.
 Se non è possibile allocare un nuovo SEMD perchè la lista di quelli liberi è vuota, restituisce TRUE.
 In tutti gli altri casi,restituisce FALSE*/
 int insertBlocked(int *semAdd, pcb_t* p){
-        semd_t *semd_target = getSemd(semAdd);
-        if(semd_target == NULL){
-            /*Il semaforo non esiste nella ASL*/
-            semd_target = allocSem(); /*semd_target ora punta al SEMD tolto dalla semdFree. Se semdFree è vuota, semd_target == NULL*/
-            if (semd_target == NULL) /*Se la semdFree è vuota restituisco TRUE*/
-                return 1;
-            semd_target->s_semAdd = semAdd;
-            insertSEMList(&semd_h, semd_target); /*Alloco semd_target nella ASL*/
-            p->p_semAdd = semAdd;
-            if(*semAdd <= 0)
-                insertProcQ(&(semd_target->s_procQ), p); /*Inserisco p nella coda di processi bloccati di semd_target*/
-            return 0;
-        }
-        else{
-            /*Il semaforo esiste già nella ASL*/
-            insertProcQ(&(semd_target->s_procQ), p);
-            p->p_semAdd = semAdd;
-            return 0;
-        }
+		semd_t *semd_target = getSemd(semAdd);
+		if(semd_target == NULL){
+			/*Il semaforo non esiste nella ASL*/
+			semd_target = allocSem(); /*semd_target ora punta al SEMD tolto dalla semdFree. Se semdFree è vuota, semd_target == NULL*/
+			if (semd_target == NULL) /*Se la semdFree è vuota restituisco TRUE*/
+				return 1;
+			semd_target->s_semAdd = semAdd;
+			insertSEMList(&semd_h, semd_target); /*Alloco semd_target nella ASL*/
+			p->p_semAdd = semAdd;
+			if(*semAdd <= 0)
+				insertProcQ(&(semd_target->s_procQ), p); /*Inserisco p nella coda di processi bloccati di semd_target*/
+			return 0;
+		}
+		else{
+			/*Il semaforo esiste già nella ASL*/
+			insertProcQ(&(semd_target->s_procQ), p);
+			p->p_semAdd = semAdd;
+			return 0;
+		}
 }
 
 
 
 /*Rimuove dalla ASL il semaforo puntato da sem*/
 semd_t *deAllocSem(semd_t **semd_h, semd_t *sem){
-    if(*semd_h == sem){
-        semd_t *removed = sem;
-        *semd_h = sem->s_next;
+	if(*semd_h == sem){
+		semd_t *removed = sem;
+		*semd_h = sem->s_next;
 
-        removed->s_semAdd = NULL;
-        removed->s_next = NULL;
-        insertSEMList(&semdFree_h, removed); /*Reinserisce il semaforo nella semdFree*/
-        return removed;
-    }
-    else
-        return deAllocSem(&((*semd_h)->s_next), sem);
+		removed->s_semAdd = NULL;
+		removed->s_next = NULL;
+		insertSEMList(&semdFree_h, removed); /*Reinserisce il semaforo nella semdFree*/
+		return removed;
+	}
+	else
+		return deAllocSem(&((*semd_h)->s_next), sem);
 }
 
 /*Ritorna il primo PCB dalla coda dei processi bloccati(s_ProcQ) associata al SEMD della ASL con chiave semAdd.
@@ -121,35 +119,35 @@ Se tale descrittore non esiste nella ASL, restituisce NULL.
 Altrimenti,restituisce l’elemento rimosso.
 Se la coda dei processi bloccati per il semaforo diventa vuota, rimuove il descrittore del semaforo corrispondente dalla ASL e lo inserisce nella coda dei semafori liberi (semdFree).*/
 pcb_t* removeBlocked(int *semAdd){
-    semd_t *semd_target = getSemd(semAdd);
-    if(semd_target){
-        /*Il semaforo è nella ASL*/
-        pcb_t *removed = removeProcQ(&(semd_target->s_procQ)); /*Rimuovo il primo PCB della sua coda di processi*/
-        if (semd_target->s_procQ == NULL)
-            deAllocSem(&semd_h, semd_target);
-        return removed;
-    }
-    else
-    return NULL;
+	semd_t *semd_target = getSemd(semAdd);
+	if(semd_target){
+		/*Il semaforo è nella ASL*/
+		pcb_t *removed = removeProcQ(&(semd_target->s_procQ)); /*Rimuovo il primo PCB della sua coda di processi*/
+		if (semd_target->s_procQ == NULL)
+			deAllocSem(&semd_h, semd_target);
+		return removed;
+	}
+	else
+		return NULL;
 }
 
 /*Restituisce il puntatore al pcb del primo processo bloccato sul semaforo, senza deaccordarlo.
 Se il semaforo non esiste restituisce NULL.*/
 pcb_t* headBlocked(int *semAdd){
-    semd_t *semd_target = getSemd(semAdd);
+	semd_t *semd_target = getSemd(semAdd);
 
-    if (semd_target)
-        return headProcQ(semd_target->s_procQ);
-    else
-        return NULL;
+	if (semd_target == NULL || semd_target->s_procQ == NULL)
+		return NULL;
+	else
+		return headProcQ(semd_target->s_procQ);
 }
 /*Rimuove il pcb puntato da p dalla coda dei processi sul semaforo a lui associato (p->p_semkey) sulla ASL.
 Se il processo non risulta bloccato (cioè è un errore), restituisce NULL, altrimenti restituisce il puntatore al processo*/
 pcb_t* outBlocked(pcb_t *p){
-    semd_t *semd_target = getSemd(p->p_semAdd);
-    pcb_t *blocked = NULL;
-    if(semd_target)
-        blocked = outProcQ(&(semd_target->s_procQ), p);
-    /*Se p non esiste nella coda del semaforo*/
-    return blocked;
+	semd_t *semd_target = getSemd(p->p_semAdd);
+	pcb_t *blocked = NULL;
+	if(semd_target)
+		blocked = outProcQ(&(semd_target->s_procQ), p);
+	/*Se p non esiste nella coda del semaforo*/
+	return blocked;
 }
--- test/pcb.c
+++ test/pcb.c
@@ -1,7 +1,5 @@
 #include "../h/pcb.h"
 
-
-
 HIDDEN pcb_t *pcbfree_h = NULL;
 
 
@@ -14,36 +12,26 @@ void insertPCBList(pcb_t **pcblist_p, pcb_t *pcb_elem)
         pcb_elem->p_next = NULL;
         return;
     }
-    else if ((*pcblist_p)->p_next == NULL)
-    {
+    else if ((*pcblist_p)->p_next == NULL) {
         /*Inserimento*/
         (*pcblist_p)->p_next = pcb_elem;
         pcb_elem->p_next = NULL;
     }
-    else insertPCBList(&((*pcblist_p)->p_next), pcb_elem);
+    else {
+    	insertPCBList(&((*pcblist_p)->p_next), pcb_elem);
+    }
 }
 
-
-void initPcbs_rec(int count)
-{
-    static pcb_t pcb_table[MAXPROC];
-
-    if (count>=MAXPROC) /*0...MAXPROC-1*/
-        return;
-    insertPCBList(&pcbfree_h, &pcb_table[count]);
-    count++;
-    initPcbs_rec(count);
+void initPcbs(void){
+	static pcb_t pcb_table[MAXPROC];
+	int i;
+	for (i = 0; i < MAXPROC; i++) {
+		insertPCBList(&pcbfree_h, &pcb_table[i]);
+	}
+    return;
 }
 
-
-void initPcbs(void)
-{
-    initPcbs_rec(0);
-}
-
-
-void freePcb(pcb_t *p)
-{
+void freePcb(pcb_t *p) {
     p->p_next = NULL;
     p->p_prev = NULL;
     p->p_child = NULL;
@@ -53,9 +41,7 @@ void freePcb(pcb_t *p)
     insertPCBList(&pcbfree_h, p);
 }
 
-
-pcb_t *allocPcb()
-{
+pcb_t* allocPcb() {
     if (pcbfree_h == NULL)
         return NULL;
     else
@@ -74,71 +60,57 @@ pcb_t *allocPcb()
     }
 }
 
-
-pcb_t* mkEmptyProcQ()
-{
+pcb_t* mkEmptyProcQ(){
     pcb_t* tmp = NULL;
     return tmp;
 }
 
-int emptyProcQ(pcb_t *tp)
-{
-    if(tp == NULL)
-    {
-        return 1;
-    } else
-    {
-        return 0;
-    }
-    return tp == NULL;
-}
-
-
-void insertProcQ(pcb_t **tp, pcb_t* p)
-{
-    if ((*tp) == NULL)
-    {
-        (*tp) = p;
-        p->p_next = p->p_prev = p;
-        (*tp) = p;
-        return;
-    }
-    else
-    {
-        pcb_t *last = (*tp)->p_prev;
-
-        pcb_t *tmp = p;
-
-        tmp->p_next = *tp;
-        tmp->p_prev = last;
-
-        last->p_next = (*tp)->p_prev = tmp;
-
-        *tp = tmp;
+int emptyProcQ(pcb_t *tp){
+    if (tp == NULL) {
+    	return 1;
+    } else {
+    	return 0;
     }
 }
 
+void insertProcQ(pcb_t **tp, pcb_t* p){
+    if ((*tp) == NULL) {
+       (*tp) = p;
+       p->p_next = p;
+       p->p_prev = p;
+       (*tp) = p;
+       return;
+   }
+   else {
+       pcb_t *last = (*tp)->p_prev;
+
+       pcb_t *tmp = p;
+
+       tmp->p_next = *tp;
+       tmp->p_prev = last;
+
+       last->p_next = (*tp)->p_prev = tmp;
+
+       *tp = tmp;
+   }
+}
 
 pcb_t* removeProcQ(pcb_t **tp)
 {
-    if (*tp == NULL)
-    {
+    if (*tp == NULL) {
         return *tp;
     }
-    else
-    {
+    else {
         /*// Declare two pointers and initialze them*/
         struct pcb_t *curr = *tp, *prev_1 = NULL;
 
         /*// Check if node is the only node in list*/
-        if (curr->p_next == *tp && prev_1 == NULL)
-        {
+        if (curr->p_prev == *tp && prev_1 == NULL) {
             (*tp) = NULL;
         }
             /*// If list has more than one node,
             // check if it is the first node*/
-        else
-        {
+        else {
             curr = (*tp)->p_prev;
             prev_1 = (*tp)->p_prev->p_prev;
 
@@ -146,31 +118,25 @@ pcb_t* removeProcQ(pcb_t **tp)
             prev_1->p_next = *tp;
             (*tp)->p_prev = prev_1;
         }
-        return curr;
+            return curr;
     }
 }
 
 
-pcb_t* outProcQ(pcb_t **tp, pcb_t* p)
-{
+pcb_t *outProcQ(pcb_t **tp, pcb_t *p){
     pcb_t *iterator = (*tp);
     pcb_t *removed = NULL;
-    if(p != NULL && !emptyProcQ((*tp)))
-    {
-		while(iterator -> p_next != p && iterator -> p_next != (*tp))
-        {
+    if(p != NULL && !emptyProcQ((*tp))){
+		while(iterator -> p_next != p && iterator -> p_next != (*tp)){
 			/* Scorro in avanti */
 			iterator = iterator -> p_next;
 		}
-		if( iterator -> p_next == p )
-        {
+		if( iterator -> p_next == p ){
 			removed = iterator -> p_next;
             /* Se la coda ha un solo elemento */
-			if(iterator -> p_next == iterator)
-            {
+			if(iterator -> p_next == iterator){
 				*(tp) = NULL;
-			} else
-            {
+			} else {
 				iterator -> p_next = removed -> p_next;
 				removed -> p_next -> p_prev = iterator;
 			}
@@ -179,31 +145,24 @@ pcb_t* outProcQ(pcb_t **tp, pcb_t* p)
     return removed;
 }
 
-
-pcb_t *headProcQ(pcb_t *tp)
-{
-    return (tp->p_prev);
+pcb_t *headProcQ(pcb_t *tp) {
+	return (tp->p_prev);
 }
 
 /**ALBERI**/
 
-int emptyChild(pcb_t *p)
-{
+int emptyChild(pcb_t *p){
     return p->p_child == NULL;
 }
 
-void insertChild(pcb_t *prnt, pcb_t *p)
-{
+void insertChild(pcb_t *prnt, pcb_t *p){
 	p->p_prnt=prnt;
-    if (emptyChild(prnt))
-    {
+    if (emptyChild(prnt)){
         prnt->p_child = p;
     }
-    else
-    {
+    else{
     	pcb_t *last = prnt -> p_child;
-    	while (last -> p_next_sib != NULL)
-        {
+    	while (last -> p_next_sib != NULL) {
     		last = last -> p_next_sib;
     	}
     	last -> p_next_sib = p;
@@ -213,17 +172,15 @@ void insertChild(pcb_t *prnt, pcb_t *p)
 }
 
 
-pcb_t* removeChild(pcb_t *p)
-{
+pcb_t* removeChild(pcb_t *p){
     if(p->p_child==NULL) return NULL;
-    else if(p->p_child->p_next_sib== NULL)
-    {
+    else if(p->p_child->p_next_sib== NULL){
         struct pcb_t *curr = p->p_child;
         p->p_child->p_prnt=NULL;
         p->p_child=NULL;
         return curr;
-    } else
-    {
+    }
+    else{
         struct pcb_t *curr = p->p_child;
         p->p_child= p->p_child->p_next_sib;
         curr->p_prnt=NULL;
@@ -234,12 +191,10 @@ pcb_t* removeChild(pcb_t *p)
 }
 
 
-pcb_t* outChild(pcb_t *p)
-{
+pcb_t* outChild(pcb_t* p){
     if(p->p_prnt==NULL) return NULL;
     else if(p->p_prnt->p_child== p) return (removeChild(p->p_prnt));
-    else if(p->p_next_sib== NULL)
-    {
+    else if(p->p_next_sib== NULL){
         struct pcb_t *curr = p;
         p->p_prev_sib->p_next_sib = NULL;
         p->p_prev_sib= NULL;
@@ -247,8 +202,7 @@ pcb_t* outChild(pcb_t *p)
         return curr;
     }
 
-    else
-    {
+    else{
     	struct pcb_t *curr = p;
         p->p_prev_sib->p_next_sib = p->p_next_sib;
         p->p_next_sib->p_prev_sib=p->p_prev_sib;
